dce3cda74f75109514e373b933bbcf4fe16ed063
ady <amdersz5g7>
2019-06-23 08:16:11 +0700

    ipv4 ipv6 support

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ fastify.get('/', function (request, reply) {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000)
+    await fastify.listen(process.env.PORT || 3000,"::")
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
