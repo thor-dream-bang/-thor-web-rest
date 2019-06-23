2d2345fc8e80de509065f1bcedb67392513f7eae
ady <amdersz5g7>
2019-06-23 07:56:58 +0700

    reset port

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ fastify.register(require('./v2/corporate'), { prefix: '/v2' })
 // Run the server!
 const start = async () => {
   try {
-    //await fastify.listen(process.env.PORT || 3000)
-    await fastify.listen(process.env.PORT)
+    await fastify.listen(process.env.PORT || 3000)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
